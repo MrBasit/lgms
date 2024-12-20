df267a290dda56a87998e21a0b57d8ccf7ef1cac
Muhammad Umar <umar43umar@gmail.com>
2024-12-20 22:33:25 +0500

    Loan&SecurityDeposits

---
+++ LGMS/Data/Model/SecurityDeposit.cs
@@ -0,0 +1,11 @@
+﻿namespace LGMS.Data.Model
+{
+    public class SecurityDeposit
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public int Amount { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ LGMS/Dto/SecurityDepositAddModel.cs
@@ -0,0 +1,12 @@
+﻿using LGMS.Data.Model;
+
+namespace LGMS.Dto
+{
+    public class SecurityDepositAddModel
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public int Amount { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
