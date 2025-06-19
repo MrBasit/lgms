e28781281edd9f3a8a15ae8954f385411bdac6b9
mrbasit <itechnician030@gmail.com>
2025-06-19 23:24:51 +0500

    download attandance api change

--- LGMS/Controllers/ExportController.cs
+++ LGMS/Controllers/ExportController.cs
@@ -154,23 +154,11 @@ namespace LGMS.Controllers
                 });
             }
 
-            if (searchModel.Year > 0)
+            if (searchModel.AttendaceFrom != null && searchModel.AttendaceTo != null)
             {
-                attendanceRecords = attendanceRecords.Where(ar => ar.Date.Year == searchModel.Year).ToList();
-            }
-            else
-            {
-                return BadRequest(new { message = "Year is required" });
+                attendanceRecords = attendanceRecords.Where(ar => ar.Date >= searchModel.AttendaceFrom && ar.Date <= searchModel.AttendaceTo).ToList();
             }
 
-            if (searchModel.Month > 0)
-            {
-                attendanceRecords = attendanceRecords.Where(ar => ar.Date.Month == searchModel.Month).ToList();
-            }
-            else
-            {
-                return BadRequest(new { message = "Month is required." });
-            }
             var recordsWithIncludedNames = new List<AttendanceRecord>();
 
             foreach (var name in searchModel.MachineNames)
